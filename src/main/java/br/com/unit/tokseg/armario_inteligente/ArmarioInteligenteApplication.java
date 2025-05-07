a052e8c2c0ed9f76c3697cf1f2817d9602b76ba9
JuliaCarolinaBV <juliacarolinabispo@gmail.com>
2025-05-07 16:20:22 -0300

    Criacao do projeto Spring Boot

---
+++ src/main/java/br/com/unit/tokseg/armario_inteligente/ArmarioInteligenteApplication.java
@@ -0,0 +1,13 @@
+package br.com.unit.tokseg.armario_inteligente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArmarioInteligenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArmarioInteligenteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/unit/tokseg/armario_inteligente/ArmarioInteligenteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.unit.tokseg.armario_inteligente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArmarioInteligenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
