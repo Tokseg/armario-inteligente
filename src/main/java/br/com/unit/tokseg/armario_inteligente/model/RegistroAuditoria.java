eb1b2beb21fdca6f9f7b2ad13262a1e891a362fa
JuliaCarolinaBV <juliacarolinabispo@gmail.com>
2025-05-08 14:12:59 -0300

    criacao da entidade RegistroAuditoria

---
+++ src/main/java/br/com/unit/tokseg/armario_inteligente/model/RegistroAuditoria.java
@@ -0,0 +1,58 @@
+package br.com.unit.tokseg.armario_inteligente.model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "registro_auditoria")
+public class RegistroAuditoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idRegistro;
+
+    @Column(nullable = false)
+    private String acao;
+
+    @Column
+    private String detalhes;
+
+    @Column(name = "data_hora", nullable = false)
+    private LocalDateTime dataHora;
+
+    public RegistroAuditoria(int idRegistro, String acao, String detalhes, LocalDateTime dataHora){
+
+        this.idRegistro = idRegistro;
+        this.acao = acao;
+        this.detalhes = detalhes;
+        this.dataHora = dataHora;
+    }
+
+    public int getIdRegistro(){
+
+        return idRegistro;
+    }
+
+    public String getAcao(){
+
+        return acao;
+
+    }
+
+    public String getDetalhes(){
+
+        return detalhes;
+    }
+
+    public LocalDateTime getDataHora(){
+
+        return dataHora;
+    }
+    
+}
