e6d38b72c83283aad0cf3b7bdc282a2a1ca2b410
Davi Souza <davissouza2021@gmail.com>
2025-05-09 15:49:33 -0300

    feat: add o model Encomenda

---
+++ src/main/java/br/com/unit/tokseg/armario_inteligente/model/Encomenda.java
@@ -0,0 +1,57 @@
+package br.com.unit.tokseg.armario_inteligente.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "Encomenda")
+public class Encomenda {
+
+    @Id
+    @Column(name = "idEncomenda")
+    private String idEncomenda;
+
+    private String descricao;
+    private String remetente;
+
+    @Column(name = "dataRecebimento")
+    private LocalDateTime dataRecebimento;
+
+    @ManyToOne
+    @JoinColumn(name = "idArmario", referencedColumnName = "idArmario")
+    //Só esperando criar o model de armario
+    private Armario idArmario;
+
+    @ManyToOne
+    @JoinColumn(name = "idUsuario", referencedColumnName = "idUsuario")
+    //Só esperando criar o model de usuário
+    private Usuario idUsuario;
+
+    public Encomenda(String idEncomenda, String descricao, String remetente, LocalDateTime dataRecebimento, Armario armario, Usuario usuario) {
+        this.idEncomenda = idEncomenda;
+        this.descricao = descricao;
+        this.remetente = remetente;
+        this.dataRecebimento = dataRecebimento;
+        this.idArmario = armario;
+        this.idUsuario = usuario;
+    }
+
+    public String getIdEncomenda() {
+        return idEncomenda;
+    }
+
+    //Só esperando criar o model de usuário
+    public Usuario getIdUsuario() {
+        return idUsuario;
+    }
+
+    //Só esperando criar o model de armario
+    public Armario getIdArmario() {
+        return idArmario;
+    }
+}
