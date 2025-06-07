e1b8f6f3dd123d1d66db7261791f0128ce8d85e9
Jean Meneses Barros <jeanmeneses@hotmail.com>
2025-06-07 08:07:09 -0300

    fix(compartimento): altera ID de Compartimento para UUID
    Corrige o repositório de Compartimento para usar UUID como tipo de ID. Essa mudança garante compatibilidade com o esquema do banco de dados.

--- src/main/java/br/com/unit/tokseg/armario_inteligente/repository/CompartimentoRepository.java
+++ src/main/java/br/com/unit/tokseg/armario_inteligente/repository/CompartimentoRepository.java
@@ -3,6 +3,7 @@ package br.com.unit.tokseg.armario_inteligente.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.unit.tokseg.armario_inteligente.model.Compartimento;
+import java.util.UUID;
 
-public interface CompartimentoRepository extends JpaRepository<Compartimento, Long> {
-} 
+public interface CompartimentoRepository extends JpaRepository<Compartimento, UUID> {
+}
